38024300a212a252e861076234d3e8f906edf3c9
Nicolas Silveira <nicosilveira07@gmail.com>
2026-05-27 04:24:27 -0300

    fix obtener vehiculos query

--- src/database/db.js
+++ src/database/db.js
@@ -1,18 +1,14 @@
-///database/db.js
-
-const    {Pool} = require ('pg');
-require('dotenv').config();
-
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
   password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: false }
-    : false
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-module.exports  = pool; 
+module.exports = pool;
